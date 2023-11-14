94efd117a43a9db2e8f5bbcc0772cccf0ec200bc
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-11-13 17:35:17 -0800

    add style and disable chat during processing

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ async function submitData() {
     const code = document.getElementById('sourceCode').value;
     const question = document.getElementById('question').value;
     const option = document.getElementById('options').value;
+    const submitChat = document.getElementById('submitChat');
 
     const awsServer = ".lambda-url.us-west-2.on.aws/";
     const endpoints = {
@@ -11,6 +12,7 @@ async function submitData() {
         'customprocess': "https://fudpixnolc7qohinghnum2nlm40wmozy" + awsServer,
         'analyze_function': "https://fubldwjkv4nau5qcnbrqilv6ba0dmkcc" + awsServer,
         'explain': "https://jorsb57zbzwcxcjzl2xwvah45i0mjuxs" + awsServer,
+        'chat': "https://o6pn7utohv362ubwb6h4ie3jgm0zleqb" + awsServer,
     };
 
     const data = {
@@ -19,6 +21,8 @@ async function submitData() {
         'session': `testemail: ${email}`,
         'organization': 'polytest.ai',
     };
+
+    submitChat.disabled = true;
 
     try {
         const response = await fetch(endpoints[option], {
@@ -30,8 +34,24 @@ async function submitData() {
             body: JSON.stringify(data)
         });
 
-        document.getElementById('response').value = JSON.stringify(response, null, 2);
+        if (!response.ok) {
+            if (response.status === 401) {
+                throw new Error(`Boost Cloud Service Permission issue.: ${response.status}`);
+            }
+            throw new Error(`Boost Cloud Service Error: ${response.status}`);
+        }
+
+        const responseData = await response.json();  // Parse JSON data from the response
+        document.getElementById('response').value = JSON.stringify(responseData, null, 2);
+
     } catch (error) {
-        document.getElementById('response').value = `${error.message}\n${error.stack}`;
+
+        document.getElementById('response').value = `Sara was unable to answer your request due to ${error.message}\n${error.stack}`;
+
+    } finally {
+
+        // Re-enable the submit button regardless of outcome
+        submitChat.disabled = false;
+
     }
 }
